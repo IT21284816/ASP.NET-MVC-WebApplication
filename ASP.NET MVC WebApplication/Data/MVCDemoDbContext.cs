8adc6fdd1de19b75b23a6e22fd966b8b54c98b58
Duhun De Silva <sandaruduhun@gmail.com>
2024-01-19 10:01:39 +0530

    Initial commit

--- ASP.NET MVC WebApplication/Data/MVCDemoDbContext.cs
+++ ASP.NET MVC WebApplication/Data/MVCDemoDbContext.cs
@@ -4,5 +4,11 @@ namespace ASP.NET_MVC_WebApplication.Data
 {
     public class MVCDemoDbContext : DbContext
     {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<> MyProperty { get; set; }
     }
 }
---
+++ ASP.NET MVC WebApplication/Models/Domain/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NET_MVC_WebApplication.Models.Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+
+    }
+}
