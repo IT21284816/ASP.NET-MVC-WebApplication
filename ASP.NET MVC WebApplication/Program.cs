83609537764634ab18bf0f71d36f594624da94ea
Duhun De Silva <sandaruduhun@gmail.com>
2024-01-20 09:42:20 +0530

    initial commit

--- ASP.NET MVC WebApplication/Program.cs
+++ ASP.NET MVC WebApplication/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MVCDemoDbContext>(options =>
-    options.UseSqlServer(""));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCDemoConnectionString")));
 
 var app = builder.Build();
 
