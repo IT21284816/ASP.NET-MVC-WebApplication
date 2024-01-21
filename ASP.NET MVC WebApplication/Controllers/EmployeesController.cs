e81529105ea3c1d045e47fa3246a704a2a59f7a4
Duhun De Silva <sandaruduhun@gmail.com>
2024-01-21 11:14:32 +0530

    initial commit

--- ASP.NET MVC WebApplication/Controllers/EmployeesController.cs
+++ ASP.NET MVC WebApplication/Controllers/EmployeesController.cs
@@ -1,13 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NET_MVC_WebApplication.Data;
+using ASP.NET_MVC_WebApplication.Models;
+using ASP.NET_MVC_WebApplication.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_MVC_WebApplication.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+                Department = addEmployeeRequest.Department,
+
+
+            };
+
+            mvcDemoDbContext.Employees.Add(employee);
+            mvcDemoDbContext.SaveChanges();
+            return RedirectToAction("Add");
+
+        }
+
     }
 }
--- ASP.NET MVC WebApplication/Models/Domain/Employee.cs
+++ ASP.NET MVC WebApplication/Models/Domain/Employee.cs
@@ -2,7 +2,7 @@
 {
     public class Employee
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public long Salary { get; set; }
