c427e4fa503f02775effffe1fc751b67b59b63d3
Duhun De Silva <sandaruduhun@gmail.com>
2024-01-21 10:00:28 +0530

    initial commit

---
+++ ASP.NET MVC WebApplication/Models/AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NET_MVC_WebApplication.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
