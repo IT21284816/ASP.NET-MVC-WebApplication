72dc0fb74cbe9a21caef8a29f123925cc1c48100
Duhun De Silva <sandaruduhun@gmail.com>
2024-01-23 10:56:25 +0530

    initial commit

--- ASP.NET MVC WebApplication/Program.cs
+++ ASP.NET MVC WebApplication/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MVCDemoDbContext>(options =>
