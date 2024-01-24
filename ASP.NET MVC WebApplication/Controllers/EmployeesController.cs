b16d9438cca14e7fbfa61399187a3690ee02e840
Duhun De Silva <sandaruduhun@gmail.com>
2024-01-24 09:19:23 +0530

    initial commit

--- ASP.NET MVC WebApplication/Controllers/EmployeesController.cs
+++ ASP.NET MVC WebApplication/Controllers/EmployeesController.cs
@@ -74,5 +74,22 @@ namespace ASP.NET_MVC_WebApplication.Controllers
             return View(employee);
         }
 
+
+        [HttpPost]
+        public async Task<IActionResult> View (UpdateEmployeeViewModel model)
+        {
+            var employee = await mvcDemoDbContext.Employees.FindAsync(model.Id);
+
+            if (employee!=null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;   
+                employee.Salary = model.Salary;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Department = model.Department;
+
+            }
+        }
+
     }
 }
